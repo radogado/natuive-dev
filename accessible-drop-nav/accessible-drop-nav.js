f82334618bfed7bcbf7ae428880773d1e31d7139
Radoslav Sharapanov <rado@me.com>
2018-01-14 20:27:34 +0200

    ...

--- accessible-drop-nav/accessible-drop-nav.js
+++ accessible-drop-nav/accessible-drop-nav.js
@@ -1,6 +1,7 @@
-// To do: on nav blur, deactivate all drop downs
 // The nav should be accessible by tab only, in addition to the arrow keys
 // Home/End keys to jump to first/last item
+
+// No-JS version works with clicking/tapping. Only JS version supports keyboard and doesn't need the inputs. So, delete them and process focusing and keyboard actions on the li element. Add aria-expanded to focused li's sub nav
 
 function closest(el, selector) { // Thanks http://gomakethings.com/ditching-jquery/
 
@@ -18,14 +19,72 @@ function closest(el, selector) { // Thanks http://gomakethings.com/ditching-jque
 
 }
 
-window.addEventListener('click', function (e) {
+// Delete all trigger inputs, add tabindex=0 to each li
+
+document.querySelectorAll('nav.drop input').forEach(function (el) {
+	
+	el.outerHTML = '';
+	
+});
+
+document.querySelector('nav.drop').addEventListener('blur', function (e) {
+	
+	console.log('blur');
+	
+});
+
+document.querySelectorAll('nav.drop li').forEach(function (el) {
+	
+	el.setAttribute('tabindex', 0);
+
+	el.addEventListener('focus', function (e) {
+		
+		if (e.target.querySelector('ul')) {
+
+			e.target.querySelector('ul').setAttribute('aria-expanded', 'true');
+
+		}
+		
+	});
+	
+	el.addEventListener('keyup', function (e) {
+		
+		if (e.key === 'Enter' && e.target.querySelector('a[href]')) {
+			
+			e.target.querySelector('a[href]').click();
+			
+		}
+		
+	});
+	
+	if (el.querySelector('ul')) {
+
+		el.setAttribute('aria-haspopup', true);
+	
+	}
+
+/*
+	if (el.querySelector('a')) {
+
+		el.querySelector('a').setAttribute('tabindex', -1);
+	
+	}
+*/
+
+	el.addEventListener('blur', function (e) {
+		
+		
+	});
+	
+});
+
+window.addEventListener('click', function (e) { // Close the nav when clicking outside
 	
 	if (!closest(e.target, 'nav.drop li')) {
-		
-		document.querySelectorAll('nav.drop input').forEach ( function (el) {
+
+		document.querySelectorAll('nav.drop ul').forEach ( function (el) {
 			
-			el.checked = false;
-			el.removeAttribute('data-checked');
+			el.removeAttribute('aria-expanded');
 			
 		});
 		
@@ -33,193 +92,18 @@ window.addEventListener('click', function (e) {
 	
 });
 
-var active_item = null;	
-
-	document.querySelectorAll('nav.drop > ul > li > input').forEach ( function (el) { // Disable standard keyboard behaviour on inputs
-		
-		el.onkeyup = el.onkeydown = el.onkeypress = function(e) {
-			
-			if (e.key === 'ArrowDown') {
-			
-				e.preventDefault();
-			
-			}
-			
-		};
-		
-	});
+document.querySelector('nav.drop').addEventListener('click', function (e) {
 	
-	function focusItem(e) {
-			
-		var el = e.target;	
-		if (el.checked && el.getAttribute('data-checked')) {
-
-			el.removeAttribute('data-checked');
-			el.checked = false;
-			
-			el.parentNode.querySelector('ul').querySelectorAll('input').forEach( function (el) {
-				
-				el.checked = false;
-				el.removeAttribute('data-checked');
-				
-			});
-			
-		} else {
-			
-			// Clear data-attribute from all other inputs
-			
-			document.querySelectorAll('input[name=' + el.name + ']').forEach( function (el) {
-				
-				el.checked = false;
-				el.removeAttribute('data-checked');
-				
-			});
-			
-			
-		}
-		
-			el.checked = true;
-			el.setAttribute('data-checked', 'true');
-			el.focus();
-		
-		
-		return false;
-
-	}
-
-	function blurEvent(e) {
-
-/*
-		if (!document.querySelector('nav.drop :focus')) {
-		console.log('blur');
-			
-			document.querySelectorAll('nav.drop [data-checked]').forEach( function (el) {
-				
-				el.checked = false;
-				el.removeAttribute('data-checked');
-				
-			});
-			
-		}
-*/
-
-		console.log(e);
+	console.log(e.target);
 	
-/*
-		if (!document.querySelector('nav.drop :focus') && e.target.getAttribute('name') === 'level-0') {
-			
-			document.querySelectorAll('nav.drop input').forEach( function (el) {
-				
-				el.checked = false;
-				el.removeAttribute('data-checked');
-				
-			});
-						
-		}
-*/
-		
-	}
+});
 
-	document.querySelectorAll('nav.drop input').forEach ( function (el) {
-		
-		el.addEventListener('click', focusItem);
-		el.addEventListener('focus', function(e) {
-
-			if (!e.target.getAttribute('data-checked')) {
-				
-				focusItem(e);
-				
-			}
-			
-		});
-		
-		el.addEventListener('blur', blurEvent);
-		
-		el.addEventListener('keyup', function (e) {
-
-			if (e.key === 'Enter') {
+document.querySelector('nav.drop').addEventListener('keyup', function (e) {
 	
-				el.previousElementSibling.click();
-			
-			}
-			
-		});
-		
-		el.addEventListener('keydown', function (e) {
-
-			if (e.key === 'ArrowDown') { // Focus on sub nav
-				
-				document.querySelectorAll('nav.drop input').forEach ( function (el) {
-					
-					el.removeEventListener('blur', blurEvent);
-					
-				});
+	console.log(e.target);
+	console.log(e.key);
 	
-				if (e.target.parentNode.querySelector('ul').querySelector('input')) {
-					
-					e.stopPropagation();
-					e.target.parentNode.querySelector('ul').querySelector('input').focus();
-				
-
-				} else { // Focus on any child ul with a[href]
-					
-					
-				}
-
-				document.querySelectorAll('nav.drop input').forEach ( function (el) {
-					
-					el.addEventListener('blur', blurEvent);
-					
-				});
-
-// 				e.target.parentNode.querySelector('ul').querySelector('input').focus();
-				
-			}
-			
-		});
-		
-	});
+	// Check for sibling or children to expand on control keys Left/Right/etc
 	
-	document.querySelectorAll('nav.drop input, nav.drop a[href]').forEach ( function (el) {
-		
-/*
-		if (el.nextElementSibling.type === 'radio') {
-			
-			el.setAttribute('tabindex', '-1');
-			
-		}
-*/
-		
-		el.addEventListener('keyup', function (e) {
-
-			if (e.key === 'Escape') {
-				
-				e.target.blur();
-				document.querySelectorAll('nav.drop input[data-checked]').forEach ( function (el) {
-				
-					el.checked = false;
-					el.removeAttribute('data-checked');
-
-				});
-				
-			}
-
-		});
-
-	});
-
-	document.querySelector('nav.drop li li:first-child > input').addEventListener('keydown', function (e) {
-		
-		if (e.key === 'ArrowUp') {
-
-			e.stopPropagation();
-			e.preventDefault();
-			e.target.parentNode.parentNode.parentNode.querySelector('input').focus();
-			return false;
-		
-		}
-		
-	});
-	
-	// When focused on a top-level item, bottom arrow key should move focus to its first child item
+});
 
