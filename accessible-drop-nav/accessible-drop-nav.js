b7132bb917e1fd62c258ff7dfa4d076430ad6944
Radoslav Sharapanov <radoslav.sharapanov@paysafe.com>
2018-01-22 16:28:22 +0200

    Keyboard-accessible drop nav

--- accessible-drop-nav/accessible-drop-nav.js
+++ accessible-drop-nav/accessible-drop-nav.js
@@ -26,6 +26,46 @@ function closest(el, target) { // Thanks http://gomakethings.com/ditching-jquery
 
 }
 
+// Real time touch detection to support devices with both touch and mouse. http://www.javascriptkit.com/dhtmltutors/sticky-hover-issue-solutions.shtml
+
+;(function(){
+
+    var isTouch = false; //var to indicate current input type (is touch versus no touch) 
+    var isTouchTimer;
+    var curRootClass = ''; //var indicating current document root class ("can-touch" or "")
+     
+    function addtouchclass(e) {
+
+        clearTimeout(isTouchTimer);
+        isTouch = true;
+        if (curRootClass != 'can-touch') { //add "can-touch' class if it's not already present
+
+            curRootClass = 'can-touch';
+            document.querySelector('html').classList.add(curRootClass);
+
+        }
+
+        isTouchTimer = setTimeout(function(){isTouch = false}, 500); //maintain "istouch" state for 500ms so removetouchclass doesn't get fired immediately following a touch event
+
+    }
+     
+    function removetouchclass(e){
+
+        if (!isTouch && curRootClass === 'can-touch'){ //remove 'can-touch' class if not triggered by a touch event and class is present
+
+            isTouch = false;
+            curRootClass = '';
+            document.querySelector('html').classList.remove(curRootClass);
+
+        }
+
+    }
+     
+    document.addEventListener('mouseover', removetouchclass, false); //this event gets called when input type is everything from touch to mouse/ trackpad
+    document.addEventListener('touchstart', addtouchclass, false); //this event only gets called when input type is touch
+
+})();
+
 /* –––  */
 
 function closeDropNavClickedOutside(e) { // Close the nav when clicking outside
